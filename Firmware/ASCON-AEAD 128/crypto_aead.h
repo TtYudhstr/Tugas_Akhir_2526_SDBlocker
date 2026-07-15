9e3750b7a1641abca9d5baccbe43b4c6f17ab5a3
TtYudhstr <titototito23@gmail.com>
2026-07-15 10:47:16 +0700

    Add files via upload

---
+++ Firmware/ASCON-AEAD 128/crypto_aead.h
@@ -0,0 +1,32 @@
+#ifndef CRYPTO_AEAD_H
+#define CRYPTO_AEAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Fungsi standar NIST untuk Enkripsi
+int crypto_aead_encrypt(
+  unsigned char *c, unsigned long long *clen,
+  const unsigned char *m, unsigned long long mlen,
+  const unsigned char *ad, unsigned long long adlen,
+  const unsigned char *nsec,
+  const unsigned char *npub,
+  const unsigned char *k
+);
+
+// Fungsi standar NIST untuk Dekripsi
+int crypto_aead_decrypt(
+  unsigned char *m, unsigned long long *mlen,
+  unsigned char *nsec,
+  const unsigned char *c, unsigned long long clen,
+  const unsigned char *ad, unsigned long long adlen,
+  const unsigned char *npub,
+  const unsigned char *k
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
